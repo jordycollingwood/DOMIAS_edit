c2808f7d9cc8eb5eba37fec70603b3aaf1297513
Jordan Collingwood <jordycollingwood@gmail.com>
2023-12-22 17:41:01 +0000

    commented out llm

--- src/domias/models/llm.py
+++ src/domias/models/llm.py
@@ -1,23 +1,23 @@
-from be_great import GReaT
-from .generator import GeneratorInterface
-import pandas as pd
-import numpy as np
+# from be_great import GReaT
+# from .generator import GeneratorInterface
+# import pandas as pd
+# import numpy as np
 
 
 
-class LLM(GeneratorInterface):
-    def __init__(self, epochs=25, batch_size=32) -> None:
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.model = GReaT(llm='distilgpt2', batch_size=self.batch_size, epochs=self.epochs)
+# class LLM(GeneratorInterface):
+#     def __init__(self, epochs=25, batch_size=32) -> None:
+#         self.epochs = epochs
+#         self.batch_size = batch_size
+#         self.model = GReaT(llm='distilgpt2', batch_size=self.batch_size, epochs=self.epochs)
 
-    def fit(self, train_data: pd.DataFrame) -> "LLM":
-        self.model.fit(train_data)
-        return self
+#     def fit(self, train_data: pd.DataFrame) -> "LLM":
+#         self.model.fit(train_data)
+#         return self
 
-    def generate(self, n: int) -> pd.DataFrame:
-        synthetic_data = self.model.sample(n_samples=n)
-        return synthetic_data
+#     def generate(self, n: int) -> pd.DataFrame:
+#         synthetic_data = self.model.sample(n_samples=n)
+#         return synthetic_data
 
 
 
