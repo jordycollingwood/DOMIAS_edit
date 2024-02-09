b246099f32e5f038b1c9ec760984b4049d3fcc9f
Jordan Collingwood <jordycollingwood@gmail.com>
2024-02-09 19:03:59 +0000

    removed wd

--- src/domias/evaluator.py
+++ src/domias/evaluator.py
@@ -14,7 +14,7 @@ from scipy.stats import multivariate_normal
 # domias absolute
 from domias.baselines import baselines, compute_metrics_baseline
 from domias.bnaf.density_estimation import compute_log_p_x, density_estimator_trainer
-from domias.metrics.wd import compute_wd
+#from domias.metrics.wd import compute_wd
 from domias.models.ctgan import CTGAN
 #from domias.models.llm import LLM
 from domias.models.generator import GeneratorInterface
@@ -200,10 +200,10 @@ def evaluate_performance(
         synth_val_set = generator.generate(synthetic_size)
 
         wd_n = min(len(synth_set), len(reference_set))
-        eval_met_on_reference = compute_wd(synth_set[:wd_n], reference_set[:wd_n])
+        #eval_met_on_reference = compute_wd(synth_set[:wd_n], reference_set[:wd_n])
         performance_logger[synthetic_size]["MIA_performance"][
             "sample_quality"
-        ] = eval_met_on_reference
+        ] = 0
 
         # get real test sets of members and non members
         X_test = np.concatenate([mem_set, non_mem_set])
